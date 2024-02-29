5d825e233d92754307636b336b219bbfc7c4decf
Saravasha <33827803+Saravasha@users.noreply.github.com>
2024-02-29 20:49:52 +0100

    wae
    eaw

--- MVC_GoshehArt/obj/Debug/net8.0/MVC_GoshehArt.AssemblyInfo.cs
+++ MVC_GoshehArt/obj/Debug/net8.0/MVC_GoshehArt.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVC_GoshehArt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73ba7bec2952c76bbb0bcad5f94726d0beae949b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32e3665ba3b281662491a1d19c9cd921ed69a3b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVC_GoshehArt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVC_GoshehArt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
