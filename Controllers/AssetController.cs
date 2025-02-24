dfff06d752f2b9d6d3b9c72c6a43802767c2f131
Saravasha <33827803+Saravasha@users.noreply.github.com>
2025-02-24 23:24:31 +0100

    aweawe

--- Controllers/AssetController.cs
+++ Controllers/AssetController.cs
@@ -19,7 +19,7 @@ namespace GoshehArtWebApp.Controllers
 
 			if (asset.ImageUp != null)
 			{
-				string uploadsFolder = Path.Combine(webHostEnvironment.WebRootPath, "imagesAsset");
+				string uploadsFolder = Path.Combine(Directory.GetCurrentDirectory(), "imagesAsset");
 				uniqueFileName = Guid.NewGuid().ToString() + "_" + asset.ImageUp.FileName;
                 string filePath = Path.Combine(uploadsFolder, uniqueFileName);
                 
@@ -33,7 +33,7 @@ namespace GoshehArtWebApp.Controllers
 				}
 			}
 
-			return Path.Combine("imagesAsset",uniqueFileName);
+			return "imagesAsset"+uniqueFileName;
 		}
 
         private readonly ApplicationDbContext _context;
