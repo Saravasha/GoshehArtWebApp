9d16843a99ecbe6afa5a26164782a222d7eb415c
Saravasha <33827803+Saravasha@users.noreply.github.com>
2025-03-06 02:11:24 +0100

    aweawe

--- obj/Release/net8.0/GoshehArtWebApp.AssemblyInfo.cs
+++ obj/Release/net8.0/GoshehArtWebApp.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GoshehArtWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7edf25588491a776c8221e96112dfb4a0bc414a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4760d190cf15f4e74f1189f55e73e41fef8bead9")]
 [assembly: System.Reflection.AssemblyProductAttribute("GoshehArtWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GoshehArtWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
