9e9c888ad071dd2eb2aa160bdfc1ce51e2b95085
Saravasha <33827803+Saravasha@users.noreply.github.com>
2025-05-27 02:25:56 +0200

    mail

--- Services/SmtpEmailSender.cs
+++ Services/SmtpEmailSender.cs
@@ -8,6 +8,7 @@ namespace GoshehArtWebApp.Services
     {
         private readonly IConfiguration _config;
         private readonly bool _isProduction;
+        private readonly bool _isStaging;
     
 
        public SmtpEmailSender(IConfiguration config)
@@ -21,6 +22,15 @@ namespace GoshehArtWebApp.Services
             {
                 _isProduction = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production";
             }
+
+            if (Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT") == "Staging")
+            {
+                _isStaging = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT") == "Staging";
+            }
+            else
+            {
+                _isStaging = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Staging";
+            }
         }
 
         public async Task SendEmailAsync(string email, string subject, string htmlMessage)
@@ -28,11 +38,11 @@ namespace GoshehArtWebApp.Services
             try
             {
                
-                string host = _isProduction ? Environment.GetEnvironmentVariable("Smtp__Host") : _config["Smtp:Host"];
-                int port = int.Parse(_isProduction ? Environment.GetEnvironmentVariable("Smtp__Port") : _config["Smtp:Port"]);
-                string username = _isProduction ? Environment.GetEnvironmentVariable("Smtp__Username") : _config["Smtp:Username"];
-                string password = _isProduction ? Environment.GetEnvironmentVariable("Smtp__Password") : _config["Smtp:Password"];
-                string from = _isProduction ? Environment.GetEnvironmentVariable("Smtp__From") : _config["Smtp:From"];
+                string host = _isProduction || _isStaging ? Environment.GetEnvironmentVariable("Smtp__Host") : _config["Smtp:Host"];
+                int port = int.Parse(_isProduction || _isStaging ? Environment.GetEnvironmentVariable("Smtp__Port") : _config["Smtp:Port"]);
+                string username = _isProduction || _isStaging ? Environment.GetEnvironmentVariable("Smtp__Username") : _config["Smtp:Username"];
+                string password = _isProduction || _isStaging ? Environment.GetEnvironmentVariable("Smtp__Password") : _config["Smtp:Password"];
+                string from = _isProduction || _isStaging ? Environment.GetEnvironmentVariable("Smtp__From") : _config["Smtp:From"];
 
                 var smtpClient = new SmtpClient(host)
                 {
