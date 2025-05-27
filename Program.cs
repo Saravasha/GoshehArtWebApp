36a85dd9da7d11ba06bf61a79da60068e18a6837
Saravasha <33827803+Saravasha@users.noreply.github.com>
2025-05-27 23:49:17 +0200

    aweawe

--- Program.cs
+++ Program.cs
@@ -203,6 +203,7 @@ if (app.Environment.IsStaging())
     var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
     Console.WriteLine(dbContext.Database.GetConnectionString());
 
+
     try
     {
         dbContext.Database.Migrate();
