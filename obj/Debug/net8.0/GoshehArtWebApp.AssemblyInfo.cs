40cd21722256a460f792adafd5044dacc2d23cc0
Saravasha <33827803+Saravasha@users.noreply.github.com>
2025-05-30 03:01:06 +0200

    updating ef core auto migration on production

--- obj/Debug/net8.0/GoshehArtWebApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/GoshehArtWebApp.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GoshehArtWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48c0d175245cdece883158711215b348d1066ac0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fc0de8c1555ae42750e5c4f818474bcf2f1af73")]
 [assembly: System.Reflection.AssemblyProductAttribute("GoshehArtWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GoshehArtWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
