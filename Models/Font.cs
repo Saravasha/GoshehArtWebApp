686fb97e18d8fd25b71dba8e61981bdf12a24796
Saravasha <33827803+Saravasha@users.noreply.github.com>
2025-06-12 01:24:49 +0200

    UI normalization, added Opacity fields to Color Model, updated the Cruds with Views and Controllers. Considering adding a font API, need to work on the design.

---
+++ Models/Font.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace GoshehArtWebApp.Models
+{
+    public class Font
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Family { get; set; }
+
+        public string Style { get; set; }
+
+        public string Weight { get; set; }
+
+        public string FileUrl { get; set; }
+
+        [NotMapped]
+        [JsonIgnore]
+        public IFormFile FileUp { get; set; }
+
+
+    }
+}
