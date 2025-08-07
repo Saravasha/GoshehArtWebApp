81354137eead228460fe0f71a05c86af0fc74b1c
Saravasha <33827803+Saravasha@users.noreply.github.com>
2025-08-08 01:59:10 +0200

    updated workflow and asserted everything is up to date, mails get sent etc. removing robot.txt on non staging envs in programcs

--- obj/Debug/net8.0/GoshehArtWebApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/GoshehArtWebApp.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GoshehArtWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2367f9aa268707396966499da24a903bba944a04")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e089a84b9dd77c783270eae80b5d6d159ed4b35")]
 [assembly: System.Reflection.AssemblyProductAttribute("GoshehArtWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GoshehArtWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
